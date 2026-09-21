32cb12bfbbc9803369ee68a59f28c8d3a803473b
varun <varun2002joshi@gmail.com>
2026-09-21 21:16:41 +1000

    chore: add terraform config for CI validation

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+﻿output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_connection_string" {
+  value     = azurerm_storage_account.storage.primary_connection_string
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+﻿variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+}
+
+variable "aks_node_count" {
+  description = "Number of AKS worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "aks_vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
